9655e1ff7ea5a8248510cfba3812103c63143ec7
AdityaAde <adidtyaad@gmail.com>
2021-07-24 21:31:18 +0700

    screen

--- lib/src/views/home_screen.dart
+++ lib/src/views/home_screen.dart
@@ -8,115 +8,279 @@ class HomeScreen extends StatelessWidget {
       body: SizedBox(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        child: Stack(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage("assets/images/pattern.png"))),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              child: Container(
-                margin: EdgeInsets.only(top: 100, left: 20, right: 20),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Find Your \nFavorite Food",
-                          style: TextStyle(
-                              fontSize: 31, fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(width: 10),
-                        Card(
-                          child: IconButton(
-                              icon: Icon(
-                                Icons.notifications_outlined,
-                                color: Colors.green,
-                              ),
-                              onPressed: () {}),
-                        )
-                      ],
-                    ),
-                    SizedBox(height: 20),
-                    Card(
-                      color: Colors.red[50],
-                      elevation: 0,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      child: TextField(
-                        cursorColor: Colors.black,
-                        decoration: new InputDecoration(
-                          prefixIcon: Icon(
-                            Icons.search,
-                            color: Colors.red[200],
-                          ),
-                          border: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                          enabledBorder: InputBorder.none,
-                          errorBorder: InputBorder.none,
-                          disabledBorder: InputBorder.none,
-                          contentPadding: EdgeInsets.only(
-                              left: 15, bottom: 11, top: 11, right: 15),
-                          hintText: "What do you want to order?",
-                          hintStyle: TextStyle(color: Colors.red[200]),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Container(
-                      width: 325,
-                      height: 150,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10)),
-                      margin: EdgeInsets.only(
-                          bottom: MediaQuery.of(context).size.height / 2 - 100),
-                      child: Image.asset(
-                        'assets/images/promo.png',
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ],
-                ),
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage("assets/images/pattern.png"))),
               ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.height,
-              child: Container(
-                padding: EdgeInsets.only(left: 15, right: 15),
-                margin: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height / 2 + 70,
-                    left: 20,
-                    right: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Nearest Restaurant",
-                          style: TextStyle(
-                              fontSize: 15, fontWeight: FontWeight.bold),
-                        ),
-                        Text("View More",
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child: Container(
+                  margin: EdgeInsets.only(top: 100, left: 20, right: 20),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Find Your \nFavorite Food",
                             style: TextStyle(
-                                color: Colors.red[200],
-                                fontSize: 12,
-                                fontWeight: FontWeight.bold))
-                      ],
-                    )
-                  ],
+                                fontSize: 31, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(width: 10),
+                          Card(
+                            child: IconButton(
+                                icon: Icon(
+                                  Icons.notifications_outlined,
+                                  color: Colors.green,
+                                ),
+                                onPressed: () {}),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Card(
+                        color: Colors.red[50],
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        child: TextField(
+                          cursorColor: Colors.black,
+                          decoration: new InputDecoration(
+                            prefixIcon: Icon(
+                              Icons.search,
+                              color: Colors.red[200],
+                            ),
+                            border: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            enabledBorder: InputBorder.none,
+                            errorBorder: InputBorder.none,
+                            disabledBorder: InputBorder.none,
+                            contentPadding: EdgeInsets.only(
+                                left: 15, bottom: 11, top: 11, right: 15),
+                            hintText: "What do you want to order?",
+                            hintStyle: TextStyle(color: Colors.red[200]),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        width: 325,
+                        height: 150,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10)),
+                        margin: EdgeInsets.only(
+                            bottom:
+                                MediaQuery.of(context).size.height / 2 - 100),
+                        child: Image.asset(
+                          'assets/images/promo.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            )
-          ],
+              SizedBox(
+                width: MediaQuery.of(context).size.height,
+                child: Container(
+                  padding: EdgeInsets.only(left: 15, right: 15),
+                  margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height / 2 + 70,
+                      left: 20,
+                      right: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Nearest Restaurant",
+                            style: TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.bold),
+                          ),
+                          Text("View More",
+                              style: TextStyle(
+                                  color: Colors.red[200],
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold))
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                  height: MediaQuery.of(context).size.height,
+                  margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height / 2 + 100,
+                      left: 35,
+                      right: 35),
+                  child: GridView.count(
+                    scrollDirection: Axis.vertical,
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                    children: [
+                      Card(
+                        shadowColor: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        elevation: 5,
+                        child: Container(
+                          child: Align(
+                            alignment: Alignment.topCenter,
+                            child: Column(
+                              children: [
+                                ClipRRect(
+                                  child: Image.asset(
+                                    'assets/images/restoran1.png',
+                                    width: 100,
+                                    height: 100,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Text(
+                                  "Healthy Food",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Card(
+                        shadowColor: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        elevation: 5,
+                        child: Container(
+                          child: Align(
+                            alignment: Alignment.topCenter,
+                            child: Column(
+                              children: [
+                                ClipRRect(
+                                  child: Image.asset(
+                                    'assets/images/restoran2.png',
+                                    width: 100,
+                                    height: 100,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Text(
+                                  "Healthy Food",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Card(
+                        shadowColor: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        elevation: 5,
+                        child: Container(
+                          child: Align(
+                            alignment: Alignment.topCenter,
+                            child: Column(
+                              children: [
+                                ClipRRect(
+                                  child: Image.asset(
+                                    'assets/images/restoran3.png',
+                                    width: 100,
+                                    height: 100,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Text(
+                                  "Healthy Food",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Card(
+                        shadowColor: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        elevation: 5,
+                        child: Container(
+                          child: Align(
+                            alignment: Alignment.topCenter,
+                            child: Column(
+                              children: [
+                                ClipRRect(
+                                  child: Image.asset(
+                                    'assets/images/restoran4.png',
+                                    width: 100,
+                                    height: 100,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Text(
+                                  "Healthy Food",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ))
+            ],
+          ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Colors.green),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, color: Colors.green),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_shopping_cart, color: Colors.green),
+            label: 'School',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.chat_bubble,
+              color: Colors.green,
+            ),
+            label: 'Settings',
+          ),
+        ],
       ),
     );
   }
